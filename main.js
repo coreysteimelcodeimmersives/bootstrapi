8cffd7dc55f3d27baaf5ef75912cc113774298ff
Corey Steimel <coreysteimel@Coreys-MacBook-Pro-Work.local>
2022-04-05 12:44:26 -0600

    using jquery to set img with http request

--- main.js
+++ main.js
@@ -1,1 +1,25 @@
-let dogImg = 'dogImg'
+// Use jQuery to declare DOMs
+let newDogButton = $('#newDogButton');
+let dogImg = $('#dogImg');
+// let body = $('body');
+// let header = $('<div></div>');
+// body.append(header);
+// header.text('HELLO');
+
+
+// use eventlistener 
+newDogButton.on('click', function(){
+// Use API to fetch random dog img
+    fetch('https://dog.ceo/api/breeds/image/random').then(
+        function(httpRes){
+            return httpRes.json();
+        }
+    ).then(
+        function(data){
+            console.log(data.message)
+            dogImg.attr("src", data.message);
+        }
+    );
+});
+
+
