f41a53a8b34882006c39b11c6ffb4bbdea161f23
Corey Steimel <coreysteimel@Coreys-MacBook-Pro-Work.local>
2022-04-05 16:04:59 -0600

    todays weather working

--- main.js
+++ main.js
@@ -3,6 +3,8 @@ let newDogButton = $('#newDogButton');
 let dogImg = $('#dogImg');
 let weatherForm = $('form');
 let cityInput = $('#cityInput');
+let weatherContent = $('#weather-content');
+let weatherCard = $('<div class="card-body weatherCard"></div>').css('margin-top', '20px', 'margin-bottom', '20px');
 
 
 // use eventlistener 
@@ -19,18 +21,36 @@ newDogButton.on('click', function(){
     );
 });
 
-weatherForm.on('submit', function(){
-    console.log(cityInput.val());
-    alert(cityInput.val())
-    // fetch(`https://goweather.herokuapp.com/weather/${cityInput.val()}`).then(
-    //     function(httpRes){
-    //         return httpRes.json();
-    //     }
-    // ).then(
-    //     function(data){
-    //         console.log(data.message);
-    //     }
-    // )
-})
+weatherForm.on('submit', function(event){
+    event.preventDefault();
+    let city = $('#cityInput').val();
+    let weatherTitle = $(`<h5 class="card-title">${city} Weather</h5>`);
+    weatherCard.empty()
+    weatherContent.append(weatherCard);
+    weatherCard.append(weatherTitle)  
+    fetch(`https://goweather.herokuapp.com/weather/${city}`).then(
+        function(httpRes){
+            return httpRes.json();
+        }
+    ).then(
+        function(data){
+            console.log(data.temperature);
+            let temp = convertCelsiusToFahrenheit(data.temperature);
+            let wind = convertKphToMph(data.wind);
+            let weatherDesc = $(`<p class="card-text">Today's weather in ${city} is ${temp} °F, ${data.description.toLowerCase()}, with ${wind.toFixed()} mph winds.</p>`);
+            weatherCard.append(weatherDesc);
+        }
+    )
+    cityInput.val('');
+});
+
+// Helper Function
+function convertCelsiusToFahrenheit(cel){
+    return (parseInt(cel) * (9/5)) + 32;
+}
+
+function convertKphToMph(kph){
+    return (parseInt(kph) / 1.609344);
+}
 
 
