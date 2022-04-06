94e7de11583d476f7d59aa22ebb0c40478432bc6
Corey Steimel <coreysteimel@Coreys-MacBook-Pro-Work.local>
2022-04-05 22:41:08 -0600

    added a dope table

--- main.js
+++ main.js
@@ -5,6 +5,15 @@ let weatherForm = $('form');
 let cityInput = $('#cityInput');
 let weatherContent = $('#weather-content');
 let weatherCard = $('<div class="card-body weatherCard"></div>').css('margin-top', '20px', 'margin-bottom', '20px');
+let weatherTable = $(`table`);
+// let thead = $('thead');
+// let tr = $('tr');
+// let caption = $('<caption>3-Day Forecast</caption>')
+let thead = $(`<caption>3-Day Forecast</caption><thead><tr>
+  <th scope="col"></th>
+  <th scope="col"></th>
+  <th scope="col"></th>
+</tr></thead>`);
 
 
 // use eventlistener 
@@ -26,6 +35,7 @@ weatherForm.on('submit', function(event){
     let city = $('#cityInput').val();
     let weatherTitle = $(`<h5 class="card-title">${city} Weather</h5>`);
     weatherCard.empty()
+    weatherTable.empty();
     weatherContent.append(weatherCard);
     weatherCard.append(weatherTitle)  
     fetch(`https://goweather.herokuapp.com/weather/${city}`).then(
@@ -46,6 +56,25 @@ weatherForm.on('submit', function(event){
                 let wind = convertKphToMph(data.wind);
                 let weatherDesc = $(`<p class="card-text">Today's weather in ${city} is ${temp.toFixed()} °F, ${data.description.toLowerCase()}, with ${wind.toFixed()} mph winds.</p>`);
                 weatherCard.append(weatherDesc);
+                weatherTable.append(thead);
+                let body = $(`<tbody>
+                <tr>
+                  <th scope="row">${getDayOfTheWeek(1)}</th>
+                  <td>${convertCelsiusToFahrenheit(data.forecast[0].temperature).toFixed()} °F</td>
+                  <td>${convertKphToMph(data.forecast[0].wind).toFixed()} mph winds</td>
+                </tr>
+                <tr>
+                  <th scope="row">${getDayOfTheWeek(2)}</th>
+                  <td>${convertCelsiusToFahrenheit(data.forecast[1].temperature).toFixed()} °F</td>
+                  <td>${convertKphToMph(data.forecast[1].wind).toFixed()} mph winds</td>
+                </tr>
+                <tr>
+                  <th scope="row">${getDayOfTheWeek(3)}</th>
+                  <td>${convertCelsiusToFahrenheit(data.forecast[2].temperature).toFixed()} °F</td>
+                  <td>${convertKphToMph(data.forecast[2].wind).toFixed()} mph winds</td>
+                </tr>
+              </tbody>`);
+              weatherTable.append(body)
             }
         }
     )
@@ -59,6 +88,12 @@ function convertCelsiusToFahrenheit(cel){
 
 function convertKphToMph(kph){
     return (parseInt(kph) / 1.609344);
+}
+
+function getDayOfTheWeek(num){
+    const weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+    const d = new Date();
+    return weekday[d.getDay()+num];
 }
 
 
